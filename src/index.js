af9c3b1bfea6577158f3faa9f41194ff631293d9
Nekolas777 <u202217485@upc.edu.pe>
2024-01-08 20:26:55 -0500

    images for mobile devices added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const d = document;
 const navbar = d.querySelector('.nav-link');
 const menuHamburguer = d.querySelector('.menu-hamburguer');
 const menuClose = d.querySelector('.menu-close');
+const cards = d.querySelectorAll('.card > img');
+// este variable almacena un tipo de dato boolean que verifica cuando el ancho de la ventana, aqui empieza en false
+let isMobile = window.matchMedia("(max-width: 605px)").matches;
 
 d.addEventListener('click', (e) => {
     if (e.target.closest('.menu-hamburguer')) {
@@ -20,16 +23,31 @@ d.addEventListener('click', (e) => {
     }
 });
 
-function toggleButtonHamburguer() {
 
-    if (menuHamburguer.style.display === 'block') {
-        menuClose.style.display = 'block';
-        menuHamburguer.style.display = 'none';
+window.addEventListener('resize', (e) => {
+
+    let currentDevice = window.matchMedia("(max-width: 605px)").matches;
+
+    // Se verifica si el tamaño de la ventnaa es de 605 o menos
+    if (currentDevice !== isMobile) {
+        isMobile = currentDevice;
+        updateCardImages(cards, isMobile)
     }
-    else if (menuClose.style.display === 'block') {
-        menuClose.style.display = 'none';
-        menuHamburguer.style.display = 'block';
-    }
+
+})
+
+
+function updateCardImages(cards, isMobile) {
+
+    cards.forEach(card => {
+        // obtiene la url actual del elemento img de la card
+        let currentURL = card.getAttribute('src');
+        // Si isMobile es true, reemplaza 'desktop' por 'mobile' en la URL actual.
+        let newURL = isMobile ? currentURL.replace('desktop', 'mobile')
+            : currentURL.replace('mobile', 'desktop');
+
+        card.setAttribute('src', newURL);
+    })
 
 }
 
